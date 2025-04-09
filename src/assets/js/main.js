110c49aefc7e5a1242ffb4f81d2c18a19b77e3a4
klimVova <87908301+klimVova@users.noreply.github.com>
2025-04-09 22:53:03 +0300

    mob menu

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,38 @@
-console.log("Hello world!");
+const bnt = document.querySelector('.lang__mode')
+const mode = document.querySelector('body')
+
+if(localStorage.getItem('langMode') === 'lang'){
+    bnt.classList.add('lang__mode--active');
+    mode.classList.add('lang');
+}
+
+bnt.onclick = function() {
+    const isBtn = bnt.classList.toggle('lang__mode--active');
+    const isMode = mode.classList.toggle('lang');
+}
+
+const bnt1 = document.querySelector('.lang__mode__item')
+const mode1 = document.querySelector('body')
+
+if(localStorage.getItem('langMode1') === 'lang1'){
+    bnt.classList.add('lang__mode--active1');
+    mode.classList.add('lang1');
+}
+
+bnt1.onclick = function() {
+    const isBtn = bnt1.classList.toggle('lang__mode--active1');
+    const isMode = mode1.classList.toggle('lang1');
+}
+
+document.querySelector('button').addEventListener('click', function() {
+    var elementOne = document.getElementById('one');
+    var elementTwo = document.getElementById('two');
+    
+    if (elementOne.classList.contains("color")) {
+      elementOne.classList.remove("color");
+      elementTwo.classList.add("color");
+    } else {
+      elementOne.classList.add("color");
+      elementTwo.classList.remove("color");
+    }
+  })
